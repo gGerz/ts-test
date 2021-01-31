8ed37c1270eab3deda7f1a6ab23620557bc6c8fc
gGerz <mr_gzag@mail.ru>
2021-01-31 16:36:26 +0700

    one more commit

--- index.ts
+++ index.ts
@@ -28,7 +28,9 @@ class CCY extends Cryptocurrency {
         super(crypto);
         this.setValue(num)
     }
-
+    getValue () : number {
+        return Number(`${this.intNum}.${this.fractNum}`)
+    }
     getIntNum () : number {
         return this.intNum
     }
@@ -37,15 +39,6 @@ class CCY extends Cryptocurrency {
     }
     getDigits () : number {
         return this.digits
-    }
-    setValue (num: number | string) : void {
-        let separator = typeof num === 'number' ? '.' : ','
-        this.intNum = parseInt((num.toString().split(separator)[0]))
-        this.fractNum = parseInt((num.toString().split(separator)[1]))
-        this.digits = (num.toString().split(separator)[1]).length
-    }
-    getValue () : number {
-        return Number(`${this.intNum}.${this.fractNum}`)
     }
     getBalance () : string {
         return `${this.intNum}.${this.fractNum} ${this.getCode()}`
@@ -64,11 +57,49 @@ class CCY extends Cryptocurrency {
             }
         }
     }
+    setValue (num: number | string) : void {
+        let separator = typeof num === 'number' ? '.' : ','
+        this.intNum = parseInt((num.toString().split(separator)[0]))
+        this.fractNum = parseInt((num.toString().split(separator)[1]))
+        this.digits = (num.toString().split(separator)[1]).length
+    }
     plusValue (value: number) : number {
         let oldValue = this.getValue()
         let newValue = oldValue + value
         this.setValue(newValue)
         return newValue
+    }
+    plusPercentValue (percent: number, setting: string = 'full') : number {
+        switch (setting) {
+            case 'full' : {
+                this.plusValue(this.getPercent(percent))
+                return this.getValue()
+            }
+            case 'int' : {
+                this.plusValue(this.getPercent(percent, 'int'))
+                return this.getValue()
+            }
+            case 'fract': {
+                this.plusValue(this.getPercent(percent, 'fract'))
+                return this.getValue()
+            }
+        }
+    }
+    minusPercentValue (percent: number, setting: string = 'full') : number {
+        switch (setting) {
+            case 'full' : {
+                this.minusValue(this.getPercent(percent))
+                return this.getValue()
+            }
+            case 'int' : {
+                this.minusValue(this.getPercent(percent, 'int'))
+                return this.getValue()
+            }
+            case 'fract': {
+                this.minusValue(this.getPercent(percent, 'fract'))
+                return this.getValue()
+            }
+        }
     }
     minusValue (value: number) : number {
         let oldValue = this.getValue()
@@ -87,8 +118,10 @@ class CCY extends Cryptocurrency {
     }
     // todo:
     // реализовать прибавление \ отнятие
+    // реализовать замена числа
     // инициализация задать число двумя отдельными параметрами
 }
+
 
 let testCrypto = {
     name: 'PLAZMA',
@@ -96,11 +129,12 @@ let testCrypto = {
 }
 
 const tmp = new CCY(testCrypto, 150.346)
-console.log(tmp.getIntNum())
-console.log(tmp.getFractNum())
-console.log(tmp.getDigits())
 console.log(tmp.getValue())
-console.log(tmp.getBalance())
-console.log(tmp.plusValue(44.27))
-console.log(tmp.getBalance())
-console.log(tmp.getPercent(10, 'fract'), '%')
+console.log(tmp.getPercent(10, 'fract'))
+console.log(tmp.plusPercentValue(10, 'fract'))
+
+// Вопросы:
+// 1) Норм практика возвращать во всех методах 'this',
+// чтобы можно было писать такие схемы как tmp.getValue().plusPercent(3).minusPercent(1)
+// и тд ?
+
